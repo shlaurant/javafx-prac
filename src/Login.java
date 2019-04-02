1c42f3df51b67cba25101cb37bf670e55f92aa90
Laurant <shklaurant@gmail.com>
2019-04-02 19:59:35 +0900

    wow! button active!

--- src/Login.java
+++ src/Login.java
@@ -2,16 +2,21 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public final class Login extends Application {
+    private final Text actionTarget = new Text();
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("JavaFX Welcome");
@@ -21,19 +26,41 @@ public final class Login extends Application {
 
     private GridPane LoginGridPane() {
         GridPane grid = gridPane();
-        grid.add(titleText(), 0, 0, 2, 1);
-        grid.add(userNameLable(), 0, 1);
-        grid.add(textField(), 1, 1);
-        grid.add(pwLable(), 0, 2);
-        grid.add(pwField(), 1, 2);
+        addLoginLabelsTo(grid);
+        grid.add(signInButton(), 1, 4);
+        grid.add(actionTarget, 1, 6);
         return grid;
+    }
+
+    private HBox signInButton() {
+        HBox signInButton = new HBox(10);
+        signInButton.setAlignment(Pos.BOTTOM_RIGHT);
+        signInButton.getChildren().add(btnToAttach());
+        return signInButton;
+    }
+
+    private Button btnToAttach() {
+        Button btn = new Button("Sign in");
+        btn.setOnAction(event -> {
+            actionTarget.setFill(Color.FIREBRICK);
+            actionTarget.setText("Pressed!");
+        });
+        return btn;
+    }
+
+    private void addLoginLabelsTo(GridPane grid) {
+        grid.add(titleText(), 0, 0, 2, 1);
+        grid.add(userNameLabel(), 0, 1);
+        grid.add(textField(), 1, 1);
+        grid.add(pwLabel(), 0, 2);
+        grid.add(pwField(), 1, 2);
     }
 
     private PasswordField pwField() {
         return new PasswordField();
     }
 
-    private Label pwLable() {
+    private Label pwLabel() {
         return new Label("Password:");
     }
 
@@ -41,7 +68,7 @@ public final class Login extends Application {
         return new TextField();
     }
 
-    private Label userNameLable() {
+    private Label userNameLabel() {
         return new Label("User Name:");
     }
 
