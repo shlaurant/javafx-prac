78fe753301b1a637f7462c1c29d0b8de5284bd71
Laurant Jo <shklaurant@gmail.com>
2019-04-03 13:40:56 +0900

    used controller

--- src/fxml/SampleController.java
+++ src/fxml/SampleController.java
@@ -1,4 +1,14 @@
 package fxml;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class SampleController {
+    @FXML
+    private Text actiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
 }
